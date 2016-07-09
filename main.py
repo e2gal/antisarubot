c2415108bcf8cb08533afb611b0344b87bae014a
e2gal <e2gal@users.noreply.github.com>
2016-07-09 15:04:24 +0700

    Rename __init__.py to bot.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python2
 
-import i2vbot
+from i2vbot import bot
 import config
 
 def main():
     print('Listening...')
 
-    botObj = i2vbot.I2VBot(config.TOKEN)
+    botObj = bot.I2VBot(config.TOKEN)
     botObj.message_loop(run_forever = True, relax = 1)
 
 if __name__ == "__main__":
