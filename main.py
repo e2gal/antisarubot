85c148524d870099f0d010623123a7363ca0c92e
e2gal <e2gal@users.noreply.github.com>
2016-07-10 10:50:39 +0700

    Add webhook support

--- main.py
+++ main.py
@@ -3,11 +3,32 @@
 from i2vbot import bot
 import config
 
+update_queue = None
+
+if config.WEBHOOK:
+    from flask import Flask, request
+    from Queue import Queue
+
+    app = Flask(__name__)
+    update_queue = Queue()  # channel between `app` and `bot`
+
+    @app.route('/' + config.TOKEN, methods = ['GET', 'POST'])
+    def pass_update():
+        update_queue.put(request.data)  # pass update to bot
+        return 'OK'
+
+    app.run(port = config.WEBHOOK_PORT, debug = True)
+
 def main():
     print('Listening...')
 
     botObj = bot.I2VBot(config.TOKEN)
-    botObj.message_loop(run_forever = True, relax = 1)
+
+    if config.WEBHOOK:
+        bot.setWebhook(config.WEBHOOK_HOST + "/" + config.TOKEN)
+        botObj.message_loop(run_forever = True, source = update_queue)
+    else:
+        botObj.message_loop(run_forever = True, relax = 1)
 
 if __name__ == "__main__":
     main()
