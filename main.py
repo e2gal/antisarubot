b3f6cc317a616c09dc696257db7ed7a88ed6c42a
e2gal <e2gal@users.noreply.github.com>
2016-07-11 11:21:52 +0700

    Webhook mode fixes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from i2vbot import bot
 import config
 
+app = None
 update_queue = None
 
 if config.WEBHOOK:
@@ -17,18 +18,18 @@ if config.WEBHOOK:
         update_queue.put(request.data)  # pass update to bot
         return 'OK'
 
-    app.run(port = config.WEBHOOK_PORT, debug = True)
-
 def main():
     print('Listening...')
 
     botObj = bot.I2VBot(config.TOKEN)
 
     if config.WEBHOOK:
-        bot.setWebhook(config.WEBHOOK_HOST + "/" + config.TOKEN)
-        botObj.message_loop(run_forever = True, source = update_queue)
+        botObj.message_loop(source = update_queue)
+        botObj.setWebhook(config.WEBHOOK_HOST + "/" + config.TOKEN)
     else:
         botObj.message_loop(run_forever = True, relax = 1)
 
 if __name__ == "__main__":
+    if config.WEBHOOK:
+        app.run(port = config.WEBHOOK_PORT, debug = True)
     main()
